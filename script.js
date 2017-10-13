790c4fb818e3020e7ee76471711bd883cd33f4af
qingxin-yuan <qingxin.yn@gmail.com>
2017-10-12 18:48:59 -0700

    js (carousel feature)added, font weight fine tuned

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function() {
+  // your jQuery code here...
+  //flickity init
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    // autoPlay: 1500,
+    wrapAround: true
+  });
+
+console.log('hello')
+
+});
