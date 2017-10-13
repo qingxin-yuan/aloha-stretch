1e84eae4b658cb24d2087be465740872bd5f3725
qingxin-yuan <qingxin.yn@gmail.com>
2017-10-13 13:08:42 -0700

    smooth scroll feature added, subscribe alerts added, minor .css layout adjusting

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
 $(function() {
-  // your jQuery code here...
   //flickity init
   $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true,
-    // autoPlay: 1500,
+    autoPlay: 1500,
     wrapAround: true
   });
 
-console.log('hello')
+  //smooth scrolling code from css tricks 
+  //https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+  $('.email-form').on('click', '.button',function(event){
+      event.preventDefault();
+
+      var emailAddress = $('.email-address').val();
+      var filter =/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+      var validation = filter.test(emailAddress);
+      if (validation){
+        alert('Thanks for subscribing!');
+      }
+      else{
+          alert('Please enter a valid email address');
+      }
+  });
+  // console.log('hello');
 
 });
